90167e2e01e79647fbfef9803a51969f2d571451
Valerimakaridze <valeriousmonk85@gmail.com>
2023-03-17 00:11:16 +0400

    switch to node:fs/promises & refactoring

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import fs from 'node:fs';
+import fs from 'node:fs/promises';
 import { scrape } from './scrape.js';
 
 const createDataFile = async () => {
-	const data = await scrape();
-
-	fs.writeFileSync('data.json', JSON.stringify(data), err => {
-		if (err) throw err;
-		console.log('Data has been written to file successfully.');
-	});
+	try {
+		const data = await scrape();
+		await fs.writeFile('data.json', JSON.stringify(data));
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 createDataFile();
--- scrape.js
+++ scrape.js
@@ -1,40 +1,40 @@
 import puppeteer from 'puppeteer';
 import parser from 'node-html-parser';
 
-const URL = 'https://widget.streamthunder.org';
-
 export async function scrape() {
+	const URL = 'https://widget.streamthunder.org';
+
 	const browser = await puppeteer.launch(); // { headless: false }
+
 	try {
 		const page = await browser.newPage();
 		await page.goto(URL);
 		// await page.setViewport({ width: 1080, height: 1024 });
+		const pageContent = await page.content();
+		if (pageContent === null) throw new Error("Can't scrape page content");
 
-		const textContent = await page.content();
-		if (textContent === null) throw new Error("Can't scrape page content");
-		const html = parser.parse(textContent);
+		const html = parser.parse(pageContent);
 		const script = html.querySelector('.ui-accordion script');
 		const scriptContent = script.textContent;
 
 		const gamesArrayVariable = 'var ev_arr = ';
 		const linksArrayVariable = 'var chan_arr = ';
+		const variableAfterLinksArray = 'var adv_1 = ';
 
 		const startIndexOfGamesArrayVariable = scriptContent.search(new RegExp(gamesArrayVariable, 'g'));
-		const endIndexOfGamesArray = scriptContent.search(new RegExp(';', 'g'));
+		const endIndexOfGamesArray = scriptContent.search(/;/g);
 		if (startIndexOfGamesArrayVariable === -1) throw new Error("Can't find ev_arr variable");
 
 		const startIndexOfLinksObjectVariable = scriptContent.search(new RegExp(linksArrayVariable, 'g'));
-		const endIndexOfLinksObject = scriptContent.search(/var adv_1 = /g);
+		const endIndexOfLinksObject = scriptContent.search(new RegExp(variableAfterLinksArray, 'g'));
 		if (startIndexOfLinksObjectVariable === -1) throw new Error("Can't find chan_arr variable");
 
 		const gamesData = scriptContent.substring(startIndexOfGamesArrayVariable, endIndexOfGamesArray).slice(gamesArrayVariable.length).trim();
 		const linksData = scriptContent
 			.substring(startIndexOfLinksObjectVariable, endIndexOfLinksObject)
-			.slice(linksArrayVariable.length)
 			.trim()
+			.slice(linksArrayVariable.length)
 			.slice(0, -1);
-
-		console.log(JSON.parse(gamesData));
 
 		return { gamesData, linksData };
 	} catch (error) {
