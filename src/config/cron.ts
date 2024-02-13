5338f630a05ae1a0c2ce5116684cab990fb171e2
Valeri Makaridze <valerianmakaridze@gmail.com>
2024-02-13 19:54:22 +0400

    Refactor cron job script and update constants

--- src/config/cron.ts
+++ src/config/cron.ts
@@ -1,48 +1,107 @@
-import { exec } from 'child_process';
+import { exec as execCb } from 'child_process';
 import cron from 'node-cron';
 import { sendSlackNotification } from './notify';
+import {
+	cronExpression,
+	cronJobError,
+	restartingDelay,
+	scriptCommand,
+	scriptErrorMessage,
+	scriptErrorPrefix,
+	scriptOutputMessage,
+	slackChannel,
+	unexpectedError,
+} from '../constants';
 
-const scriptCommand = 'pnpm run save && pnpm run upload';
-const cronExpression = '*/30 * * * *';
-const restartingDelay = 10000;
-
-let timeoutId;
-
-const runScript = () => {
-	exec(scriptCommand, (error, stdout, stderr) => {
-		if (error) {
-			sendSlackNotification('#back-end', `Cron job notification (line 14): Error running script: ${error.message}`);
-			console.error(`Cron job notification (line 15): Error running script: ${error.message}`);
-			timeoutId = setTimeout(() => {
-				sendSlackNotification('#back-end', 'Cron job notification (line 17): Restarting script...');
-				console.log('Cron job notification (line 18): Restarting script...');
-				runScript();
-			}, restartingDelay);
-			return;
-		}
-		console.log(`Cron job notification (line 23): Script output: ${stdout}`);
-		console.error(`Cron job notification (line 24): Script error: ${stderr}`);
-	});
-};
+let timeoutId: NodeJS.Timeout | null = null;
 
 export const cronJob = () => {
 	try {
 		runScript();
-		cron.schedule(cronExpression, () => runScript());
+		cron.schedule(cronExpression, runScript);
 	} catch (error) {
-		if (error instanceof Error) {
-			console.error(`Cron job notification (line 34): Error starting cron: ${error.message}`);
-			sendSlackNotification('#back-end', `Cron job notification (line 35): Error starting cron: ${error.message}`);
-			timeoutId = setTimeout(() => {
-				sendSlackNotification('#back-end', 'Cron job notification (line 37): Restarting node-cron...');
-				console.log('Cron job notification (line 38): Restarting node-cron...');
-				cronJob();
-			}, restartingDelay);
-			throw new Error(`Cron job notification (line 41): Error starting cron: ${error.message}`);
-		} else {
-			console.log('Cron job notification (line 43): unexpected error: ', error);
-			sendSlackNotification('#back-end', `Cron job notification (line 44): An unexpected error occurred: ${error}`);
-			throw new Error(`Cron job notification (line 44): An unexpected error occurred: ${error}`);
-		}
+		errorHandler(error, cronJobError, cronJob);
+		throw error;
 	}
 };
+
+function runScript() {
+	execCb(scriptCommand, (error, stdout, stderr) => {
+		if (error) {
+			errorHandler(error, scriptErrorPrefix, runScript);
+			return;
+		}
+		console.log(`${scriptOutputMessage} ${stdout}`);
+		console.error(`${scriptErrorMessage} ${stderr}`);
+	});
+}
+
+function errorHandler(error: unknown, message: string, restartFunction: () => void) {
+	const errorMessage = isInstanceOfError(error) ? `${message} ${error.message}` : `${unexpectedError} ${String(error)}`;
+
+	sendSlackNotification(slackChannel, errorMessage);
+	console.error(errorMessage);
+
+	if (timeoutId) clearTimeout(timeoutId);
+
+	timeoutId = setTimeout(() => {
+		sendSlackNotification(slackChannel, 'Restarting...');
+		console.log('Restarting...');
+		restartFunction();
+	}, restartingDelay);
+}
+
+function isInstanceOfError(error: unknown): error is Error {
+	return error instanceof Error;
+}
+
+//! Old code
+
+// import { exec } from 'child_process';
+// import cron from 'node-cron';
+// import { sendSlackNotification } from './notify';
+
+// const scriptCommand = 'pnpm run save && pnpm run upload';
+// const cronExpression = '*/30 * * * *';
+// const restartingDelay = 10000;
+
+// let timeoutId;
+
+// const runScript = () => {
+// 	exec(scriptCommand, (error, stdout, stderr) => {
+// 		if (error) {
+// 			sendSlackNotification('#back-end', `Cron job notification (line 14): Error running script: ${error.message}`);
+// 			console.error(`Cron job notification (line 15): Error running script: ${error.message}`);
+// 			timeoutId = setTimeout(() => {
+// 				sendSlackNotification('#back-end', 'Cron job notification (line 17): Restarting script...');
+// 				console.log('Cron job notification (line 18): Restarting script...');
+// 				runScript();
+// 			}, restartingDelay);
+// 			return;
+// 		}
+// 		console.log(`Cron job notification (line 23): Script output: ${stdout}`);
+// 		console.error(`Cron job notification (line 24): Script error: ${stderr}`);
+// 	});
+// };
+
+// export const cronJob = () => {
+// 	try {
+// 		runScript();
+// 		cron.schedule(cronExpression, () => runScript());
+// 	} catch (error) {
+// 		if (error instanceof Error) {
+// 			console.error(`Cron job notification (line 34): Error starting cron: ${error.message}`);
+// 			sendSlackNotification('#back-end', `Cron job notification (line 35): Error starting cron: ${error.message}`);
+// 			timeoutId = setTimeout(() => {
+// 				sendSlackNotification('#back-end', 'Cron job notification (line 37): Restarting node-cron...');
+// 				console.log('Cron job notification (line 38): Restarting node-cron...');
+// 				cronJob();
+// 			}, restartingDelay);
+// 			throw new Error(`Cron job notification (line 41): Error starting cron: ${error.message}`);
+// 		} else {
+// 			console.log('Cron job notification (line 43): unexpected error: ', error);
+// 			sendSlackNotification('#back-end', `Cron job notification (line 44): An unexpected error occurred: ${error}`);
+// 			throw new Error(`Cron job notification (line 44): An unexpected error occurred: ${error}`);
+// 		}
+// 	}
+// };
