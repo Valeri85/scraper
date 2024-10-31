132bfafdec2de5bb5a49bfa383be46ed3829c6e9
Valerimakaridze <valerianmakaridze@gmail.com>
2024-10-31 20:46:04 +0400

    bug fux: remove html entity

--- src/config/scrape.ts
+++ src/config/scrape.ts
@@ -20,7 +20,10 @@ export async function scrape() {
 
 		const script = html.querySelector('.ui-accordion script');
 		if (!script) throw new Error("Can't select <script></script> tag!");
-		const scriptContent = script.textContent.replace(/&#305;/gi, '').replace(/&#246;/gi, '');
+		const scriptContent = script.textContent
+			.replace(/&#305;/gi, '')
+			.replace(/&#246;/gi, '')
+			.replace(/&#228;/gi, '');
 
 		const startIndexOfGamesArrayVariable = scriptContent.search(new RegExp(GAMES_ARRAY_VARIABLE, 'g'));
 		const endIndexOfGamesArray = scriptContent.search(/;/g);
